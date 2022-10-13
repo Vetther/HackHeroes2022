39deaf203728666843ddd0264a22c8923bb0de4d
Dawidu7 <109083134+Dawidu7@users.noreply.github.com>
2022-10-14 00:23:53 +0200

    Google Autocomplete

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,23 @@
+import React, { useState } from 'react'
+import Autocomplete from "react-google-autocomplete"
+
 export default function App() {
+  const [address, setAddress] = useState('')
+
+  console.log(address)
+
   return (
-    <></>
+    <Autocomplete
+      apiKey={'AIzaSyCmgoNY4M_yFr_gE703rDfc7RLkzfgHflA'}
+      onPlaceSelected={(place) => {
+        // console.log(place)
+        setAddress(place.formatted_address)
+      }}
+      options={{
+        types: ["address"],
+        // fields: ["address_components", "geometry"],
+        componentRestrictions: { country: "pl" },
+      }}
+    />
   )
 }
