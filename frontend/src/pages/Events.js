806dd16e8185eb6c570d53589ccd212bd96f0385
Dawidu7 <109083134+Dawidu7@users.noreply.github.com>
2022-10-18 18:54:22 +0200

    Events Page Cleanup

--- frontend/src/pages/Events.js
+++ frontend/src/pages/Events.js
@@ -3,116 +3,18 @@ import AuthContext from '../AuthContext'
 import { Button, Modal } from 'react-daisyui'
 
 import Event from '../components/Event'
-import ModalInput from '../components/ModalInput'
-import { ErrorResponse } from '@remix-run/router'
+import EventModal from '../components/EventModal'
 
 export default function Events() {
   const [events, setEvents] = useState([])
   const [modal, setModal] = useState(false)
-  const [address, setAddress] = useState('')
-  const [modalValues, setModalValues] = useState({
-    img: '',
-    title: '',
-    summary: '',
-    description: '',
-    datetime: '',
-  })
-  const [errors, setErrors] = useState({
-    img: {
-      valid: false,
-      message: '',
-    },
-    title: {
-      valid: false,
-      message: '',
-    },
-    summary: {
-      valid: false,
-      message: '',
-    },
-    description: {
-      valid: false,
-      message: '',
-    },
-    datetime: {
-      valid: false,
-      message: '',
-    }, 
-  })
-  const { user, authTokens } = useContext(AuthContext)
+  const { user } = useContext(AuthContext)
 
-  const getEvents = () => {
+  useEffect(() => {
     fetch('http://141.147.1.251:5000/api/v1/events')
     .then(response => response.json())
     .then(data => setEvents([...data.data.content]))
-  }
-
-  useEffect(() => {
-    getEvents()
   }, [])
-
-  const isCorrectImageURL = () => {
-    if(!modalValues.img.match('(https?:\/\/(?:www\.|(?!www))[a-zA-Z0-9][a-zA-Z0-9-]+[a-zA-Z0-9]\.[^\s]{2,}|www\.[a-zA-Z0-9][a-zA-Z0-9-]+[a-zA-Z0-9]\.[^\s]{2,}|https?:\/\/(?:www\.|(?!www))[a-zA-Z0-9]+\.[^\s]{2,}|www\.[a-zA-Z0-9]+\.[^\s]{2,})')) {
-      setErrors({...errors, img: {valid: false, message: 'Niepoprawny URL'}})
-    }
-    else {
-      setErrors({...errors, img: {valid: true, message: ''}})
-    }
-  }
-
-  const isCorrectTitle = () => {
-    setErrors({...errors, title: {valid: true, message: ''}})
-  }
-
-  const isCorrectSummary = () => {
-    setErrors({...errors, summary: {valid: true, message: ''}})
-  }
-
-  const isCorrectDescription = () => {
-    setErrors({...errors, description: {valid: true, message: ''}})
-  }
-
-  const isCorrectDatetime = () => {
-    setErrors({...errors, datetime: {valid: true, message: ''}})
-  }
-
-  const isDisabled = () => {
-    return Object.values(errors).some(error => !error.valid)
-  }
-
-  const resetModal = () => {
-    setAddress('')
-    setModalValues({
-      img: '',
-      title: '',
-      summary: '',
-      description: '',
-      datetime: '',
-    })
-    setModal(false)
-  }
-
-  const addEvent = async () => {
-    await fetch('http://141.147.1.251:5000/api/v1/event/create', {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-        'Authorization': `Bearer ${authTokens.access_token}`,
-      },
-      body: JSON.stringify({
-        imageUrl: modalValues.img,
-        title: modalValues.title,
-        address: address,
-        shortDescription: modalValues.summary,
-        longDescription: modalValues.description,
-        eventTime: Math.floor(new Date(modalValues.datetime).getTime() / 1000),
-        creator_id: user.id,
-        tagId: [1],
-      })
-    })
-    getEvents()
-    resetModal()
-  }
 
   return (
     <>
@@ -145,66 +47,7 @@ export default function Events() {
           </div>
         </div>
       </div>
-      <Modal open={modal} onClickBackdrop={() => setModal(false)}>
-        <Modal.Header className='flex justify-between'>
-          <p className='font-bold text-2xl'>Stwórz Wydarzenie</p>
-          <Button size='sm' onClick={() => setModal(false)}>X</Button>
-        </Modal.Header>
-        <Modal.Body className='flex flex-col gap-y-4'>
-          <ModalInput 
-            title='Zdjęcie(url)' 
-            required 
-            type='text' 
-            value={modalValues.img} 
-            onChange={e => setModalValues({...modalValues, img: e.target.value})} 
-            onBlur={isCorrectImageURL} 
-            error={errors.img} 
-          />
-          <ModalInput 
-            title='Tytuł' 
-            required 
-            type='text' 
-            value={modalValues.title} 
-            onChange={e => setModalValues({...modalValues, title: e.target.value})} 
-            onBlur={isCorrectTitle} 
-            error={errors.title} 
-          />
-          <ModalInput 
-            title='Adres' 
-            required 
-            type='searchbar' 
-            onChange={setAddress} 
-          />
-          <ModalInput 
-            title='Krótki Opis' 
-            required 
-            type='text' 
-            value={modalValues.summary} 
-            onChange={e => setModalValues({...modalValues, summary: e.target.value})} 
-            onBlur={isCorrectSummary} 
-            error={errors.summary} 
-          />
-          <ModalInput 
-            title='Opis' 
-            required 
-            type='textarea' 
-            value={modalValues.description} 
-            onChange={e => setModalValues({...modalValues, description: e.target.value})} 
-            onBlur={isCorrectDescription} 
-            error={errors.description}
-          />
-          <ModalInput 
-            title='Data Wydarzenia' 
-            required 
-            type='datetime-local' 
-            value={modalValues.datetime} 
-            onChange={e => setModalValues({...modalValues, datetime: e.target.value})} 
-            onBlur={isCorrectDatetime} 
-            error={errors.datetime} 
-          />
-          <Button color='primary' disabled={isDisabled()} onClick={addEvent}>Stwórz</Button>
-        </Modal.Body>
-      </Modal>
+      <EventModal open={modal} setOpen={setModal} />
     </>
   )
 }
--- frontend/src/pages/UserEvents.js
+++ frontend/src/pages/UserEvents.js
@@ -3,10 +3,12 @@ import AuthContext from '../AuthContext'
 import { Button } from 'react-daisyui'
 
 import UserEvent from '../components/UserEvent'
+import EventModal from '../components/EventModal'
 
 export default function UserEvents() {
   const { user } = useContext(AuthContext)
   const [events, setEvents] = useState([])
+  const [modal, setModal] = useState(false)
 
   useEffect(() => {
     fetch('http://141.147.1.251:5000/api/v1/events')
@@ -15,18 +17,21 @@ export default function UserEvents() {
   }, [])
 
   return (
-    <div className="flex justify-center items-center h-full">
-      <div className='bg-base-100 border border-base-300 p-6 w-3/4 rounded-lg'>
-        <div className="flex justify-between items-center">
-          <p className="text-primary text-3xl font-bold">Twoje Wydarzenia</p>
-          <Button color='primary'>Stwórz</Button>
-        </div>
-        <div className={`${events.length !== 0 && 'mt-6'}`}>
-          {events.map((event, i) => (
-            <UserEvent key={i} event={event} bg={i % 2 === 0 ? '300' : '100'} />
-          ))}
+    <>
+      <div className="flex justify-center items-center h-full">
+        <div className='bg-base-100 border border-base-300 p-6 w-3/4 rounded-lg'>
+          <div className="flex justify-between items-center">
+            <p className="text-primary text-3xl font-bold">Twoje Wydarzenia</p>
+            <Button color='primary' onClick={() => setModal(true)}>Stwórz</Button>
+          </div>
+          <div className={`${events.length !== 0 && 'mt-6'}`}>
+            {events.map((event, i) => (
+              <UserEvent key={i} event={event} bg={i % 2 === 0 ? '300' : '100'} />
+            ))}
+          </div>
         </div>
       </div>
-    </div>
+      <EventModal open={modal} setOpen={setModal} />
+    </>
   )
 }
