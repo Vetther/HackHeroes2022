0987b94875a15919e76f84ac0f20799d6529a4dd
Oskar Wolny <osk4r.wolny@gmail.com>
2022-10-20 00:26:18 +0200

    Update Tab.js

--- frontend/components/Tab.js
+++ frontend/components/Tab.js
@@ -17,11 +17,11 @@ const Tab = ({ title, data }) => {
         {data.map(tab => (
           <li
             key={tab.name}
-            className={`${tab.path !== router.pathname.replace("[user]", user.sub) ? 'text-base-content/70' : 'border-r-4 border-primary text-base-content'} mb-1 pr-4`}
+            className={`${tab.path !== router.pathname.replace("[user]", user?.sub) ? 'text-base-content/70' : 'border-r-4 border-primary text-base-content'} mb-1 pr-4`}
           >
               <Link href={tab.path} className='p-2'>
                   <a className='flex'>
-                      <span className={`flex ${tab.path !== router.pathname.replace("[user]", user.sub) ? 'text-gray-500' : 'text-primary'}`}>
+                      <span className={`flex ${tab.path !== router.pathname.replace("[user]", user?.sub) ? 'text-gray-500' : 'text-primary'}`}>
                         {tab.icon}
                       </span>
                       <span>{tab.name}</span>
