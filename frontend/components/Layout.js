9ef5f2fbf2594eda2e217b7a908973774e513bc8
Oskar Wolny <osk4r.wolny@gmail.com>
2022-10-22 19:31:33 +0200

    Frontend fixes

--- frontend/components/Layout.js
+++ frontend/components/Layout.js
@@ -2,11 +2,15 @@ import { AlertProvider } from "../contexts/alert"
 
 import Sidebar from "./Sidebar"
 import Alert from "./Alert"
+import React from "react";
 
 const Layout = ({ children }) => {
+
+    const sidebar = React.useMemo( () => <Sidebar/>, [] );
+
   return (
     <div className='lg:flex bg-base-200 min-h-screen'>
-      <Sidebar />
+        {sidebar}
       <AlertProvider>
         <div className="w-full min-h-screen">
           <Alert />
