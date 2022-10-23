b7d5eda58b139186e71aa18d54f67af7d78d3366
Dawidu7 <109083134+Dawidu7@users.noreply.github.com>
2022-10-23 13:26:55 +0200

    Poll Vote

--- frontend/pages/polls.js
+++ frontend/pages/polls.js
@@ -1,15 +1,28 @@
-import { useState } from "react"
+import { useContext, useState } from "react"
 import { Radio, Button } from "react-daisyui"
+import Router from 'next/router'
+
+import AuthContext from "../contexts/auth"
 
 import Box from "../components/Box"
 import PollModal from "../components/PollModal"
 
 const Polls = ({ polls }) => {
+  const { user, tokens } = useContext(AuthContext)
   const [ modal, setModal ] = useState(false)
   const [ radio, setRadio ] = useState({ id: -1, selected: -1 })
 
-  const vote = () => {
+  const vote = async () => {
+    const response = await fetch(`http://141.147.1.251:5000/api/v1/poll/${radio.id}/choice/${radio.selected}/vote`, {
+      headers: {
+        'Authorization': `Bearer ${tokens.access_token}`,
+      }
+    })
+    const data = await response.json()
 
+    if(data.success) {
+      Router.reload()
+    }
   }
 
   return (
@@ -29,15 +42,34 @@ const Polls = ({ polls }) => {
                 })}
               </p>
               <p className='text-sm mb-4'>{poll.description}</p>
-              <div className="flex flex-col gap-y-3 mb-4">
-                {poll.choices.map(choice => (
-                  <div key={choice.id} className="flex items-center hover:cursor-pointer hover:text-base-content/70 w-fit" onClick={() => setRadio({ id: poll.id, selected: choice.id })}>
-                    <Radio name={poll.id} checked={radio.id === poll.id && radio.selected === choice.id} readOnly />
-                    <p className='ml-2'>{choice.name}</p>
-                  </div>
-                ))}
-              </div>
-              <Button size='sm' color='primary' disabled={radio.id !== poll.id} onClick={vote}>Zagłosuj</Button>
+              {!poll.choices.some(choice => choice.voters.some(vote => vote.id === user?.id))
+                ? <>
+                    <div className="flex flex-col gap-y-3 mb-4">
+                      {poll.choices.sort((a, b) => a.name < b.name ? -1 : a.name > b.name ? 1 : 0).map(choice => (
+                        <div key={choice.id} className="flex items-center hover:cursor-pointer hover:text-base-content/70 w-fit" onClick={() => setRadio({ id: poll.id, selected: choice.id })}>
+                          <Radio name={poll.id} checked={radio.id === poll.id && radio.selected === choice.id} readOnly />
+                          <p className='ml-2'>{choice.name}</p>
+                        </div>
+                      ))}
+                    </div>
+                    <Button size='sm' color='primary' disabled={radio.id !== poll.id} onClick={vote}>Zagłosuj</Button>
+                  </>
+                : <div className='flex flex-col gap-y-2'>
+                    {poll.choices.map(choice => (
+                      <div key={choice.id} className="flex justify-between">
+                        <div className="w-3/4 bg-base-300 rounded-full relative">
+                          {choice.voters.length / poll.choices.reduce((sum, choice) => sum + choice.voters.length, 0) > 0 && 
+                            <div 
+                              className="bg-primary text-sm rounded-full h-full" 
+                              style={{ width: `${choice.voters.length / poll.choices.reduce((sum, choice) => sum + choice.voters.length, 0) * 100}%` }}
+                            ></div>
+                          }
+                          <span className="ml-1 absolute top-0 text-base-100">{choice.voters.length / poll.choices.reduce((sum, choice) => sum + choice.voters.length, 0) * 100}%</span>
+                        </div>
+                        <p>{choice.name}</p>
+                      </div>
+                    ))}
+                  </div>}
             </Box>
           ))}
         </div>
